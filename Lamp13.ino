f4f273b930ab33396924f7e8dd8a780d641abd43
Real-VeerSandhu <56776466+Real-VeerSandhu@users.noreply.github.com>
2019-11-15 20:08:14 -0500

    Add files via upload

---
+++ Lamp13.ino
@@ -0,0 +1,23 @@
+#include <CapacitiveSensor.h>
+CapacitiveSensor capSensor = CapacitiveSensor(4,2);
+
+int threshold = 100;
+const int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  long sensorValue = capSensor.capacitiveSensor(30);
+  Serial.println(sensorValue);
+
+  if(sensorValue > threshold) {
+    digitalWrite(ledPin, HIGH);  
+  }
+  else {
+    digitalWrite(ledPin, LOW);  
+  }
+  delay(10);
+}
---
+++ Logo14.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+   Serial.write(analogRead(A0)/4);
+
+   delay(1);
+
+}
